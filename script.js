e782557c08d6c5b07d0303afd9a78997fe889136
Angroba <30315654+Angroba@users.noreply.github.com>
2021-07-02 17:27:09 +0300

    Add Loader
    Add loader from wc3 school
    to check how it will work
    add function loading/complete for this

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//Show Loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function complete() {
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
+
 // Show New Quote
 function newQuote() {
+    loading()
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteText.textContent = quote.text;
+    
     //check if author field is blank
     if (!quote.author) {
         authorText.textContent = 'Unknown';
@@ -22,11 +35,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    //set the quote,  Hide loader
+    quoteText.textContent = quote.text;
+    complete()
 
 }
 
 // Get Quotes From API
 async function getQuotes () {
+    loading()
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
